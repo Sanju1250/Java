ff9edd5db1c39a1fc4227b598824f40b60561745
Sanju1250 <sanjudombale414@gmail.com>
2017-03-29 13:35:32 +0530

    Sixth Program

--- Factors.java
+++ Factors.java
@@ -1,13 +1,10 @@
-import java.util.Iterator;
-import java.util.Set;
-import java.util.HashSet;
 import java.util.Scanner;
 class Factors
 {
 	public static void main(String args[])
 	{
 	Scanner sc=new Scanner(System.in);
-	Set s=new HashSet();
+	//Set s=new HashSet();
 	System.out.println("enter the number to find prime Factors");
 	int Number=sc.nextInt();
 	for(int i=2;i<=Number/2;i++)
@@ -25,16 +22,11 @@ class Factors
 	}
 	if(temp>i/2)
 	{	
-	s.add(i);
+	System.out.println(i);
 	}
 
 	}
 	}
-	Iterator it=s.iterator();
-	while(it.hasNext())
-	{
-	Object o=it.next();
-	System.out.println(o);
-	}
+
 }
 }
